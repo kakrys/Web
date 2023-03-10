24bd670a46f1280454359a7690fe31530b478388
kakrys <107796607+kakrys@users.noreply.github.com>
2023-03-10 20:37:23 +0200

    Update laba2.php

--- laba2/laba2.php
+++ laba2/laba2.php
@@ -1,7 +1,6 @@
 <?php
 //1
 $very_bad_unclear_name = "15 chicken wings";
-// Write your code here:
 $order = &$very_bad_unclear_name;
 $order .= " and coca cola";
 echo "\nYour order is: $very_bad_unclear_name.";
